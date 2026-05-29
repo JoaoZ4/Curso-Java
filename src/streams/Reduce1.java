2c61213287e99ae86f175bf9314244e312db6843
Joao Zangaro Ferneda <joaozangaro@gmail.com>
2026-05-29 09:42:06 -0300

    Realizado o exercicio de Reduce com Stream API

--- src/streams/Reduce1.java
+++ src/streams/Reduce1.java
@@ -1,8 +1,19 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
 public class Reduce1 {
 
     static void main() {
-        
+
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        BinaryOperator<Integer> soma = (ac, n) -> ac + n;
+
+        int total = nums.stream().reduce(soma).get();
+
+        System.out.println(total);
     }
 }
